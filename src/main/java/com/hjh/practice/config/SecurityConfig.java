276536628dc37fc3fa338eed2cb550221b6b4724
hjh <ktorn21@naver.com>
2026-01-29 22:29:31 +0900

    스프링시큐리티 초기설정

---
+++ src/main/java/com/hjh/practice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.hjh.practice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.formLogin(Customizer.withDefaults());
+
+        http.logout(Customizer.withDefaults());
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/css/**", "/js/**").permitAll()
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
